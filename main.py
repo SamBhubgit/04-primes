d147f09bcd0e387c214c57b0ce8b644ad9d557f7
Samuel-BOUCHER <samuel.boucher@edu.esiee.fr>
2025-11-06 10:30:44 +0000

    isPrime

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
+"""Ce programme vérifie si les nombres entre 0 et 99 sont des nombres premier"""
 from math import sqrt
 
 #### Fonction secondaire
 
-
 def isprime(p):
-
-    # votre code ici
-
+    """Cette fonction vérifie si un nombre est un nombre premier"""
+    if p==1:
+        print(""+str(p)+" : False")
+        return False
+    if p<1:
+        print(""+str(p)+" : False")
+        return False
+    for i in range(2,int(sqrt(p))+1):
+        if p%i==0:
+            print(str(p)+" = "+str(i)+" x "+str(p//i)+" : False")
+            return False
+    print(""+str(p)+" : True")
     return True
 
 #### Fonction principale
 
 
 def main():
-
+    """Cette fonction vérifie si les nombres entre 0 et 99 sont des nombres premier"""
     # vos appels à la fonction secondaire ici
 
     for n in range(100):
